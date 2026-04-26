bf38b2391364230f4fd3480e65112c93f82c5f0a
limerain18 <farrelg72@gmail.com>
2026-04-26 21:21:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ function loadPhoto(input, n) {
 // ─────────────────────────────────────────────────────
 // ── SUPABASE CONFIG — fill in your own values below ──
 // ─────────────────────────────────────────────────────
-const SUPABASE_URL  = 'https://umgcvllvhnbnutmlwdvc.supabase.co';
-const SUPABASE_ANON = 'sb_publishable_v9914Xcm3ap3fO84i1OZJA_yuYaGYb8';
+const SUPABASE_URL  = 'https://aflgjgczgthoqbeopslb.supabase.co';
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFmbGdqZ2N6Z3Rob3FiZW9wc2xiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxODQzODYsImV4cCI6MjA5Mjc2MDM4Nn0.hQGXq2nz7XD235xffPI2rvMwcjZeX6hsuGUvX54wOjw';
 // Table needed: responses (id, name, message, created_at)
 
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
